bba31259ceecddb7df7b51a178637bfa1e4a38d2
Paraponera <aharder@c1r7s2.42luxembourg.lu>
2026-09-03 18:01:29 +0200

    handler chan start

--- source/handler_chan.cpp
+++ source/handler_chan.cpp
@@ -1,11 +1,61 @@
 void    join(Client *sender, std::string text)
 {
-
+    std::stringstream ss(text);
+    std::vector<std::string> splitted;
+    std::string word;
+    while (ss >> word)
+        splitted.push_back(word);
+    try
+    {
+        if (splitted[1][0] != '#')
+            throw SyntaxErrorException();
+        splitted[1].erase(0, 1);
+        try
+        {
+            Channel channel = sender->_server->getChannel(splitted[1]);
+        }
+        catch (IrcException &e)
+        {
+            if (e.errorCode() == 403)
+            {
+                if (!splitted[2])
+                {
+                    sender->_server->createChannel(sender, splitted[1])
+                    return;
+                }
+                else if (splitted[2][0] != ':')
+                    throw SyntaxErrorException();
+                splitted[2].erase(0, 1);
+                sender->_server->createChannel(sender, splitted[1], splitted[2]);
+                return;
+            }
+            else
+                throw e;
+        }
+        if (!splitted[2])
+            channel.join(sender);
+        else if (splitted[2][0] != ':')
+            throw SyntaxErrorException();
+        splitted[2].erase(0, 1);
+        channel.join(sender, splitted[2]);
+    }
+    catch (IrcException &e)
+    {
+        sender->_server->ircERROR(sender, e.errorCode(), splitted);
+    }
 };
 
 void    part(Client *sender, std:string text)
 {
-
+    std::stringstream ss(text);
+    std::vector<std::string> splitted;
+    std::string word;
+    while (ss >> word)
+        splitted.push_back(word);
+    try
+    {
+        
+    }
 };
 
 void    privmsg(Client *sender, std:string text)
