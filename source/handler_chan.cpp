70aa464f48109cf5769f78a8a2e6347f68e43707
Paraponera <aharder@c1r7s2.42luxembourg.lu>
2026-09-03 16:01:11 +0200

    o

---
+++ source/handler_chan.cpp
@@ -0,0 +1,34 @@
+void    join(Client *sender, std::string text)
+{
+
+};
+
+void    part(Client *sender, std:string text)
+{
+
+};
+
+void    privmsg(Client *sender, std:string text)
+{
+
+};
+
+void    kick(Client *sender, std:string text)
+{
+
+};
+
+void    invite(Client *sender, std:string text)
+{
+
+};
+
+void    topic(Client *sender, std:string text)
+{
+
+};
+
+void    mode(Client *sender, std:string text)
+{
+
+};
